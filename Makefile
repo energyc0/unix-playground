d24eb0a2cc39c8c438d774ec7e32f96481a24a4b
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-15 21:03:34 +0300

    added elf_to_read.c file

--- Makefile
+++ Makefile
@@ -6,12 +6,17 @@ SRCDIR:=src
 
 SRC:=$(shell ls $(SRCDIR)/*.c)
 SRC:=$(patsubst $(SRCDIR)/%.c, %.c, $(SRC))
-SRC:=$(filter-out utils.c, $(SRC))
+DO_NOT_COMPILE:=utils.c
+
+SRC:=$(filter-out $(DO_NOT_COMPILE), $(SRC))
 
 OUT:=$(patsubst %.c, $(BUILDDIR)/%.out, $(SRC))
 OBJ:=$(BUILDDIR)/utils.o
 
 all: $(OBJ) $(OUT)
+
+$(BUILDDIR)/elf_to_read_file.out: $(SRCDIR)/elf_to_read_file.c
+	gcc -o $@ $^
 
 $(OBJ): $(SRCDIR)/utils.c $(SRCDIR)/utils.h
 	mkdir -p $(BUILDDIR)
---
+++ src/elf_to_read_file.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char globBuf[65536];
+int primes[] = { 2, 3, 5, 7 };
+
+static int square(int x)
+{
+    int result;
+    result = x * x;
+    return result;
+}
+
+/* Сегмент неинициализированных данных */
+/* Сегмент инициализированных данных */
+/* Размещается в фрейме для square() */
+/* Размещается в фрейме для square() */
+/* Возвращаемое значение передается через регистр */
+
+static void doCalc(int val)
+/* Размещается в фрейме для doCalc() */
+{
+    printf("The square of %d is %d\n", val, square(val));
+    if (val < 1000) {
+        int t;
+        /* Размещается в фрейме для doCalc() */
+        t = val * val * val;
+        printf("The cube of %d is %d\n", val, t);
+    }
+}
+int main(int argc, char *argv[])
+/* Размещается в фрейме для main() */
+{
+    static int key = 9973;
+    /* Сегмент инициализированных данных */
+    static char mbuf[10240000];
+    /* Сегмент неинициализированных данных */
+    char *p;
+    /* Размещается в фрейме для main() */
+    p = malloc(1024);
+    /* Указывает на память в сегменте кучи */
+    doCalc(key);
+    exit(EXIT_SUCCESS);
+}
