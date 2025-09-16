ec64299943fde5a56cde297f61c3586db0397553
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-16 12:24:44 +0300

    longjmp after function return exercise

---
+++ src/longjmp_fail.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <setjmp.h>
+
+static jmp_buf env;
+
+void f1(int val)
+{
+    printf("f1(%d)\n", val);
+    longjmp(env, val);
+}
+
+void f2(int val)
+{
+    printf("f2(%d)\n", val);
+    f1(val - 1);
+}
+
+void f3(int val)
+{
+    printf("f3(%d)\n", val);
+    f2(val - 1);
+}
+
+void foo(int val) 
+{
+    if (setjmp(env) == 0) {
+        printf("foo(%d) setjmp() == 0\n", val);
+    } else {
+        printf("foo(%d) cookies!\n", val);
+    }
+}
+
+int main(void)
+{
+    switch (setjmp(env)) {
+        case 0: f3(3); break;
+        case 1: f2(3); break;
+        case 2: f1(3); break;
+        case 3: break;
+        default: 
+        fprintf(stderr, "Undefined value\n");
+        return 1;
+    }
+    printf("done\n");
+
+    foo(99);
+
+    int someval = 11;
+    printf("someval = %d\n", someval);
+    printf("longjmp()\n");
+
+    longjmp(env, 3);
+    return 0;
+}
